a6df32b2caf571b4eec79ba9aa9e3132e4b854dc
Elias <rist24082001@gmail.com>
2020-03-29 13:32:35 +0200

    Design und Inhalt eingefügt

--- Rist_Camping/Rist_Camping/App_Start/BundleConfig.cs
+++ Rist_Camping/Rist_Camping/App_Start/BundleConfig.cs
@@ -19,11 +19,7 @@ namespace Rist_Camping
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
