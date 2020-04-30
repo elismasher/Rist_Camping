bc533af1c234118f042d048f60048840324ca8a4
Elias <rist24082001@gmail.com>
2020-04-30 15:40:07 +0200

    Reservierung aufsteigend sortieren 2

--- Rist_Camping/Rist_Camping/Models/db/RepositoryReservation.cs
+++ Rist_Camping/Rist_Camping/Models/db/RepositoryReservation.cs
@@ -87,7 +87,7 @@ namespace Rist_Camping.Models.db
             List<Reservation> reservations = new List<Reservation>();
 
             DbCommand cmdSelect = this._connection.CreateCommand();
-            cmdSelect.CommandText = "SELECT * FROM reservations ORDER BY arrivalFate ASC";
+            cmdSelect.CommandText = "SELECT * FROM reservations ORDER BY arrivalDate ASC";
 
             using (DbDataReader reader = cmdSelect.ExecuteReader())
             {
